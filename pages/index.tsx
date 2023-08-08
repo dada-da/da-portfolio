99495b193dda7ac047e47e544de834c285761c52
caoduc.anh <anhcd@etc.vn>
2023-08-08 16:55:01 +0700

    feat: social icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
-import Header from '@/components/Header';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+
+import Header from '@/components/Header';
+import Hero from '@/components/Hero';
 
 const Home: NextPage = () => {
   return (
@@ -10,6 +12,10 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
+
+      <section id="about-me">
+        <Hero />
+      </section>
     </div>
   );
 };
