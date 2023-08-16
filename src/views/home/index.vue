1d70821c9e9ef40a5759ba8eeb34b4590c5431b4
da-da <anhcd@etc.vn>
2023-08-16 15:30:36 +0700

    init project

---
+++ src/views/home/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <NLayout>
+    <NLayoutContent>
+      <div>test</div>
+    </NLayoutContent>
+  </NLayout>
+</template>
+
+<script setup lang="ts">
+import { NLayout, NLayoutContent } from 'naive-ui'
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,26 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import vue from '@vitejs/plugin-vue';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+function pathResolve(dir: string) {
+  return resolve(process.cwd(), '.', dir);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue()],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+    alias: [
+      // /@/xxxx => src/xxxx
+      {
+        find: /\/@\//,
+        replacement: pathResolve('src') + '/',
+      },
+      // /#/xxxx => types/xxxx
+      {
+        find: /\/#\//,
+        replacement: pathResolve('types') + '/',
+      },
+    ],
+  },
+});
